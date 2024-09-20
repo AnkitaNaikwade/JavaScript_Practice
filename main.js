e1152129623f94078d4a58de01fb356590123ffb
Ankita Naikwade <naikwaadeankita40@gmail.com>
2024-09-20 14:43:57 +0530

    main file

---
+++ main.js
@@ -0,0 +1,9 @@
+import {add, sub, mult, div} from "./mymodule.js"
+
+const a = 10;
+const b = 20;
+
+console.log("Addition : " + add(a,b) + "<br>");
+console.log("Substraction : " + sub(a,b) + "<br>");
+console.log("Multiplication : " + mult(a,b) + "<br>");
+console.log("Division : " + div(a,b) + "<br>");
